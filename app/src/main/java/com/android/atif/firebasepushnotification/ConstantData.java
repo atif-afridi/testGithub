82162e63ef412c8b28acd824b76a81b517f70f00
Atif-Liaqat <atifafridi1990@gmail.com>
2017-01-03 20:04:44 +0500

    first comit

---
+++ app/src/main/java/com/android/atif/firebasepushnotification/ConstantData.java
@@ -0,0 +1,16 @@
+package com.android.atif.firebasepushnotification;
+
+/**
+ * Created by Atif-Liaqat on 12/20/2016.
+ */
+
+public class ConstantData {
+
+    public static final String FIREBASE_CHILD_SEARCHED_LOCATION = "searchedLocation";
+    public static final String FIREBASE_CHILD_DEVICES = "Devices";
+
+
+
+
+
+}
